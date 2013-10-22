c814c21749aa81c04d6c7377a40fea9539e4acc5
Bjornn Borg <bfelix@amil.com.br>
2013-10-22 15:12:46 -0200

    criando estrutura para avaliar formação de pares nos jogos

---
+++ src/main/java/br/com/six2six/psycho/model/mao/AvaliadorPar.java
@@ -0,0 +1,38 @@
+package br.com.six2six.psycho.model.mao;
+
+import static br.com.six2six.psycho.model.mao.Jogo.PAR;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import br.com.six2six.psycho.model.AvaliadorMao;
+import br.com.six2six.psycho.model.Carta;
+import br.com.six2six.psycho.model.DadosAvaliacaoJogo;
+import br.com.six2six.psycho.model.Mao;
+import br.com.six2six.psycho.model.ValorFace;
+import br.com.six2six.psycho.model.mao.util.AgrupadorHelper;
+
+public class AvaliadorPar implements AvaliadorMao {
+	
+	@Override
+	public DadosAvaliacaoJogo getDadosAvaliacao(Mao mao) {
+		Map<ValorFace, List<Carta>> agrupadoPorValorFace = AgrupadorHelper.agruparPorValorFace(mao);
+		boolean matches = false;
+		List<Carta> cartasJogo = new ArrayList<Carta>();
+		if (agrupadoPorValorFace.keySet().size() == 4) {
+			for(ValorFace valorFace : agrupadoPorValorFace.keySet()) {
+				if (agrupadoPorValorFace.get(valorFace).size() != 2) {
+					continue;
+				}
+				matches = true;
+				cartasJogo = agrupadoPorValorFace.get(valorFace);
+			}
+		}
+		
+		return new DadosAvaliacaoJogo(PAR, matches, cartasJogo);
+	}
+
+	
+
+}
---
+++ src/test/java/br/com/six2six/psycho/model/mao/AvaliadorParTest.java
@@ -0,0 +1,25 @@
+package br.com.six2six.psycho.model.mao;
+
+import static br.com.six2six.psycho.model.mao.Jogo.PAR;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import br.com.six2six.psycho.model.Carta;
+import br.com.six2six.psycho.model.DadosAvaliacaoJogo;
+import br.com.six2six.psycho.model.Mao;
+
+public class AvaliadorParTest {
+	
+	@Test
+	public void deveIdentificarPar() {
+		AvaliadorPar par = new AvaliadorPar();
+		DadosAvaliacaoJogo dadosAvaliacao = par.getDadosAvaliacao(new Mao(Carta.from("3S", "4H", "5D", "4D", "AC")));
+		assertEquals(PAR, dadosAvaliacao.getJogo());
+		assertTrue(dadosAvaliacao.fazJogo());
+		assertEquals(Carta.from("4H"), dadosAvaliacao.getCartasJogo().get(0));
+		assertEquals(Carta.from("4D"), dadosAvaliacao.getCartasJogo().get(1));
+	}
+
+}
