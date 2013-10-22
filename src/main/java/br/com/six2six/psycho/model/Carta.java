523ce13a8d51f9591607ab9714d4c08fdce23468
Bjornn Borg <bfelix@amil.com.br>
2013-10-22 12:16:25 -0200

    adicionando equals e permitindo criar várias cartas no fromValue

--- src/main/java/br/com/six2six/psycho/model/Carta.java
+++ src/main/java/br/com/six2six/psycho/model/Carta.java
@@ -1,11 +1,15 @@
 package br.com.six2six.psycho.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode(of={"valorFace", "naipe"})
 public class Carta implements Comparable<Carta> {
 	
 	private static final Pattern REGEX_TUPLA = Pattern.compile("([A2-9TJQK])([CDSH])");
@@ -36,6 +40,14 @@ public class Carta implements Comparable<Carta> {
 			return new Carta(ValorFace.from(matcher.group(1)), Naipe.from(matcher.group(2)));
 		}
 		throw new IllegalArgumentException("Tupla inválida");
+	}
+	
+	public static List<Carta> from(String... tupla) {
+		List<Carta> cartas = new ArrayList<Carta>();
+		for (String tuplaDaVez : tupla) {
+			cartas.add(Carta.from(tuplaDaVez));
+		}
+		return cartas;
 	}
 
 }
