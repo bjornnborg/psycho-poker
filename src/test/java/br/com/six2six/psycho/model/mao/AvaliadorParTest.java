41de361b15d23d89826fb65b3876bcd6cf5a9f6f
Bjornn Borg <bfelix@amil.com.br>
2013-10-22 17:41:26 -0200

    testes para identificar pares corretamente

--- src/test/java/br/com/six2six/psycho/model/mao/AvaliadorParTest.java
+++ src/test/java/br/com/six2six/psycho/model/mao/AvaliadorParTest.java
@@ -1,8 +1,10 @@
 package br.com.six2six.psycho.model.mao;
 
+
 import static br.com.six2six.psycho.model.mao.Jogo.PAR;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -19,6 +21,33 @@ public class AvaliadorParTest {
 		assertTrue(dadosAvaliacao.fazJogo());
 		assertEquals(Carta.from("4H"), dadosAvaliacao.getCartasJogo().get(0));
 		assertEquals(Carta.from("4D"), dadosAvaliacao.getCartasJogo().get(1));
+	}
+	
+	@Test
+	public void naoDeveIdentificarDoisParesComoPar() {
+		AvaliadorPar par = new AvaliadorPar();
+		DadosAvaliacaoJogo dadosAvaliacao = par.getDadosAvaliacao(new Mao(Carta.from("3S", "4H", "5D", "4D", "5C")));
+		assertEquals(PAR, dadosAvaliacao.getJogo());
+		assertFalse(dadosAvaliacao.fazJogo());
+		assertEquals(0, dadosAvaliacao.getCartasJogo().size());
+	}
+	
+	@Test
+	public void naoDeveIdentificarTrincaComoPar() {
+		AvaliadorPar par = new AvaliadorPar();
+		DadosAvaliacaoJogo dadosAvaliacao = par.getDadosAvaliacao(new Mao(Carta.from("4S", "4H", "5D", "4D", "5C")));
+		assertEquals(PAR, dadosAvaliacao.getJogo());
+		assertFalse(dadosAvaliacao.fazJogo());
+		assertEquals(0, dadosAvaliacao.getCartasJogo().size());
+	}	
+	
+	@Test
+	public void naoDeveIdentificarFourComoPar() {
+		AvaliadorPar par = new AvaliadorPar();
+		DadosAvaliacaoJogo dadosAvaliacao = par.getDadosAvaliacao(new Mao(Carta.from("3S", "4H", "4C", "4D", "4S")));
+		assertEquals(PAR, dadosAvaliacao.getJogo());
+		assertFalse(dadosAvaliacao.fazJogo());
+		assertEquals(0, dadosAvaliacao.getCartasJogo().size());
 	}
 
 }
