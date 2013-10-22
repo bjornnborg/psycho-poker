331746a86b87f9eeb7e0a36094821a2ea4a2bbf5
Bjornn Borg <bfelix@amil.com.br>
2013-10-22 20:05:18 -0200

    Detectando sequencias numéricas

--- src/main/java/br/com/six2six/psycho/model/mao/AvaliadorSequenciaNumerica.java
+++ src/main/java/br/com/six2six/psycho/model/mao/AvaliadorSequenciaNumerica.java
@@ -1,5 +1,6 @@
 package br.com.six2six.psycho.model.mao;
 
+import static br.com.six2six.psycho.model.ValorFace.As;
 import static br.com.six2six.psycho.model.mao.Jogo.SEQUENCIA_NUMERICA;
 
 import java.util.ArrayList;
@@ -21,12 +22,24 @@ public class AvaliadorSequenciaNumerica implements AvaliadorMao {
 		List<Carta> cartasJogo = new ArrayList<Carta>();
 		if (agrupadoPorValorFace.keySet().size() == 5) {
 			int gap = mao.getCartas().get(4).getValorFace().getPeso() - mao.getCartas().get(0).getValorFace().getPeso();
-			matches = gap == (mao.getCartas().size() - 1); 
+			matches = gap == (mao.getCartas().size() - 1);
+			
+			if (!matches && As == mao.getCartas().get(4).getValorFace()) {
+				gap = mao.getCartas().get(3).getValorFace().getPeso() - mao.getCartas().get(0).getValorFace().getPeso();
+				matches = gap == (mao.getCartas().size() - 2);
+				
+				if (matches) {
+					int pesoProvisorioAs = 1;
+					matches = (mao.getCartas().get(0).getValorFace().getPeso() - pesoProvisorioAs) == 1;
+				}
+			}
+			
 		}
 		if (matches) {
 			cartasJogo = mao.getCartas();
 		}
 		return new DadosAvaliacaoJogo(SEQUENCIA_NUMERICA, matches, cartasJogo);
 	}
+	
 
 }
--- src/test/java/br/com/six2six/psycho/model/mao/AvaliadorSequenciaNumericaTest.java
+++ src/test/java/br/com/six2six/psycho/model/mao/AvaliadorSequenciaNumericaTest.java
@@ -3,6 +3,7 @@ package br.com.six2six.psycho.model.mao;
 import static br.com.six2six.psycho.model.mao.Jogo.SEQUENCIA_NUMERICA;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -18,6 +19,33 @@ public class AvaliadorSequenciaNumericaTest {
 		assertEquals(SEQUENCIA_NUMERICA, dadosAvaliacao.getJogo());
 		assertTrue(dadosAvaliacao.fazJogo());
 		assertEquals(5, dadosAvaliacao.getCartasJogo().size());
+	}
+	
+	@Test
+	public void deveIdentificarSequenciaComecandoComAs() {
+		AvaliadorSequenciaNumerica sequencia = new AvaliadorSequenciaNumerica();
+		DadosAvaliacaoJogo dadosAvaliacao = sequencia.getDadosAvaliacao(new Mao(Carta.from("3S", "2H", "5D", "4D", "AS")));
+		assertEquals(SEQUENCIA_NUMERICA, dadosAvaliacao.getJogo());
+		assertTrue(dadosAvaliacao.fazJogo());
+		assertEquals(5, dadosAvaliacao.getCartasJogo().size());
+	}
+	
+	@Test
+	public void deveIdentificarSequenciaTerminandoComAs() {
+		AvaliadorSequenciaNumerica sequencia = new AvaliadorSequenciaNumerica();
+		DadosAvaliacaoJogo dadosAvaliacao = sequencia.getDadosAvaliacao(new Mao(Carta.from("TS", "KH", "AD", "QD", "JS")));
+		assertEquals(SEQUENCIA_NUMERICA, dadosAvaliacao.getJogo());
+		assertTrue(dadosAvaliacao.fazJogo());
+		assertEquals(5, dadosAvaliacao.getCartasJogo().size());
+	}	
+	
+	@Test
+	public void naoDeveDeixarAsConfundirSequencia() {
+		AvaliadorSequenciaNumerica sequencia = new AvaliadorSequenciaNumerica();
+		DadosAvaliacaoJogo dadosAvaliacao = sequencia.getDadosAvaliacao(new Mao(Carta.from("3S", "4H", "5D", "6D", "AS")));
+		assertEquals(SEQUENCIA_NUMERICA, dadosAvaliacao.getJogo());
+		assertFalse(dadosAvaliacao.fazJogo());
+		assertEquals(0, dadosAvaliacao.getCartasJogo().size());
 	}
 
 }
