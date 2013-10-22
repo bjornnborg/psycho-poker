27f0b9d948b9185d97404a30ec33e92251def291
Bjornn Borg <bfelix@amil.com.br>
2013-10-22 12:15:06 -0200

    criando modelo para representar cada jogo

---
+++ src/test/java/br/com/six2six/psycho/model/mao/Jogo.java
@@ -0,0 +1,14 @@
+package br.com.six2six.psycho.model.mao;
+
+public enum Jogo {
+
+	MAIOR_CARTA,
+	PAR,
+	DOIS_PARES,
+	TRINCA,
+	SEQUENCIA_NUMERICA,
+	SEQUENCIA_NAIPE,
+	FULL_HOUSE,
+	FOUR,
+	STRAIGHT_FLUSH
+}
