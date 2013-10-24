85d0e58c24aac0419d66d4b642f278dffab64c1b
Bjornn Borg <bfelix@amil.com.br>
2013-10-24 16:44:55 -0200

    identificando full houses

---
+++ src/main/java/br/com/six2six/psycho/model/mao/AvaliadorFullHouse.java
@@ -0,0 +1,31 @@
+package br.com.six2six.psycho.model.mao;
+
+import static br.com.six2six.psycho.model.mao.Jogo.FULL_HOUSE;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import br.com.six2six.psycho.model.AvaliadorMao;
+import br.com.six2six.psycho.model.Carta;
+import br.com.six2six.psycho.model.DadosAvaliacaoJogo;
+import br.com.six2six.psycho.model.ValorFace;
+import br.com.six2six.psycho.model.mao.util.AgrupadorHelper;
+
+public class AvaliadorFullHouse implements AvaliadorMao {
+
+	@Override
+	public DadosAvaliacaoJogo getDadosAvaliacao(Mao mao) {
+		Map<ValorFace, List<Carta>> agrupadoPorValorFace = AgrupadorHelper.agruparPorValorFace(mao);
+		boolean matches = agrupadoPorValorFace.keySet().size() == 2;
+		List<Carta> cartasJogo = new ArrayList<Carta>();
+		
+		if (matches) {
+			cartasJogo = mao.getCartas();
+		}
+		
+		return new DadosAvaliacaoJogo(FULL_HOUSE, matches, cartasJogo);
+	}
+	
+
+}
---
+++ src/test/java/br/com/six2six/psycho/model/mao/AvaliadorFullHouseTest.java
@@ -0,0 +1,33 @@
+package br.com.six2six.psycho.model.mao;
+
+import static br.com.six2six.psycho.model.mao.Jogo.FULL_HOUSE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import br.com.six2six.psycho.model.Carta;
+import br.com.six2six.psycho.model.DadosAvaliacaoJogo;
+
+public class AvaliadorFullHouseTest {
+	
+	@Test
+	public void deveIdentificarFullHouse() {
+		AvaliadorFullHouse fullHouse = new AvaliadorFullHouse();
+		DadosAvaliacaoJogo dadosAvaliacao = fullHouse.getDadosAvaliacao(new Mao(Carta.from("3S", "8S", "3C", "8H", "3D")));
+		assertEquals(FULL_HOUSE, dadosAvaliacao.getJogo());
+		assertTrue(dadosAvaliacao.fazJogo());
+		assertEquals(5, dadosAvaliacao.getCartasJogo().size());
+	}
+	
+	@Test
+	public void naoDeveIdentificarSequencia() {
+		AvaliadorFullHouse fullHouse = new AvaliadorFullHouse();
+		DadosAvaliacaoJogo dadosAvaliacao = fullHouse.getDadosAvaliacao(new Mao(Carta.from("3S", "8S", "3C", "8H", "JD")));
+		assertEquals(FULL_HOUSE, dadosAvaliacao.getJogo());
+		assertFalse(dadosAvaliacao.fazJogo());
+		assertEquals(0, dadosAvaliacao.getCartasJogo().size());
+	}
+
+}
