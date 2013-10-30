eb6813b64b584314e1715fd631b36584808e10e4
Bjornn Borg <bfelix@amil.com.br>
2013-10-30 21:03:01 -0200

    descobrindo par como melhor jogo

--- src/test/java/br/com/six2six/psycho/model/PsychoTest.java
+++ src/test/java/br/com/six2six/psycho/model/PsychoTest.java
@@ -2,6 +2,8 @@ package br.com.six2six.psycho.model;
 
 import static br.com.six2six.psycho.model.Naipe.Ouros;
 import static br.com.six2six.psycho.model.ValorFace.As;
+import static br.com.six2six.psycho.model.ValorFace.Nove;
+import static br.com.six2six.psycho.model.mao.Jogo.PAR;
 import static br.com.six2six.psycho.model.mao.Jogo.MAIOR_CARTA;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -23,5 +25,20 @@ public class PsychoTest {
 		assertEquals(As, maiorJogo.getCartasJogo().get(0).getValorFace());
 		assertEquals(Ouros, maiorJogo.getCartasJogo().get(0).getNaipe());
 	}
+	
+	@Test
+	public void descobrirMelhorJogoComoPar() {
+		Mao mao = new Mao(Carta.from("6C", "9C", "8C", "2D", "7C")); 
+		Monte monte = Monte.from("2H", "TC", "4C", "9S", "AH");
+		
+		DadosAvaliacaoJogo maiorJogo = new Psycho().descobrirMaiorJogo(mao, monte);
+		assertEquals(PAR, maiorJogo.getJogo());
+		assertTrue(maiorJogo.fazJogo());
+		assertEquals(2, maiorJogo.getCartasJogo().size());
+		assertEquals(Nove, maiorJogo.getCartasJogo().get(0).getValorFace());
+		assertEquals(Nove, maiorJogo.getCartasJogo().get(1).getValorFace());
+	}	
+	
+//	Mão: 6C 9C 8C 2D 7C Monte: 2H TC 4C 9S AH Melhor Jogo: one-pair (1 par)
 
 }
