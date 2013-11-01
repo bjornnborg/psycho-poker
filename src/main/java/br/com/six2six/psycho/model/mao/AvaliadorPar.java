9e5c9f107ac7be65393fd04a8ce6b1960be9a288
Bjornn Borg <bfelix@amil.com.br>
2013-11-01 14:52:26 -0200

    melhorando dsl para descobrir pares

--- src/main/java/br/com/six2six/psycho/model/mao/AvaliadorPar.java
+++ src/main/java/br/com/six2six/psycho/model/mao/AvaliadorPar.java
@@ -1,34 +1,26 @@
 package br.com.six2six.psycho.model.mao;
 
+
 import static br.com.six2six.psycho.model.mao.Jogo.PAR;
+import static br.com.six2six.psycho.model.mao.Mao.comQuantidade;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import br.com.six2six.psycho.model.AvaliadorMao;
 import br.com.six2six.psycho.model.Carta;
 import br.com.six2six.psycho.model.DadosAvaliacaoJogo;
-import br.com.six2six.psycho.model.ValorFace;
-import br.com.six2six.psycho.model.mao.util.AgrupadorHelper;
 
 public class AvaliadorPar implements AvaliadorMao {
 	
 	@Override
 	public DadosAvaliacaoJogo getDadosAvaliacao(Mao mao) {
-		Map<ValorFace, List<Carta>> agrupadoPorValorFace = AgrupadorHelper.agruparPorValorFace(mao.cartas().todas());
 		boolean matches = false;
 		List<Carta> cartasJogo = new ArrayList<Carta>();
-		if (agrupadoPorValorFace.keySet().size() == 4) {
-			for(ValorFace valorFace : agrupadoPorValorFace.keySet()) {
-				if (agrupadoPorValorFace.get(valorFace).size() != 2) {
-					continue;
-				}
-				matches = true;
-				cartasJogo = agrupadoPorValorFace.get(valorFace);
-			}
+		if (mao.possui(4).gruposDeValor()) {
+			matches = true;
+			cartasJogo = mao.cartas().doGrupo(comQuantidade(2));			
 		}
-		
 		return new DadosAvaliacaoJogo(PAR, matches, cartasJogo);
 	}
 
