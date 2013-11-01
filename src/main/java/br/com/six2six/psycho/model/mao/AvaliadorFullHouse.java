0cd5285ebca38e36873b0d475deab330ea21d84f
Bjornn Borg <bfelix@amil.com.br>
2013-11-01 14:54:21 -0200

    melhorando dsl para descobrir full house

--- src/main/java/br/com/six2six/psycho/model/mao/AvaliadorFullHouse.java
+++ src/main/java/br/com/six2six/psycho/model/mao/AvaliadorFullHouse.java
@@ -4,29 +4,21 @@ import static br.com.six2six.psycho.model.mao.Jogo.FULL_HOUSE;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import br.com.six2six.psycho.model.AvaliadorMao;
 import br.com.six2six.psycho.model.Carta;
 import br.com.six2six.psycho.model.DadosAvaliacaoJogo;
-import br.com.six2six.psycho.model.ValorFace;
-import br.com.six2six.psycho.model.mao.util.AgrupadorHelper;
 
 public class AvaliadorFullHouse implements AvaliadorMao {
 
 	@Override
 	public DadosAvaliacaoJogo getDadosAvaliacao(Mao mao) {
-		Map<ValorFace, List<Carta>> agrupadoPorValorFace = AgrupadorHelper.agruparPorValorFace(mao.cartas().todas());
 		boolean matches = false;
 		List<Carta> cartasJogo = new ArrayList<Carta>();
 		
-		if (agrupadoPorValorFace.keySet().size() == 2) {
-			for(ValorFace valor : agrupadoPorValorFace.keySet()) {
-				if (agrupadoPorValorFace.get(valor).size() == 3) {
-					matches = true;
-					break;
-				}
-			}
+		if (mao.possui(2).gruposDeValor() && mao.possui(3).cartasEmAlgumGrupo()) {
+			matches = true;
+			cartasJogo = mao.cartas().todas();
 		}
 		
 		if (matches) {
